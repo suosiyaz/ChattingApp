bed7365c52fd99cc7030f0379fd0a2dbc4336054
MOHAMMED SHIYAS P A <siyazputhancode@gmail.com>
2020-05-04 00:33:39 +0530

    Initial Commit - Walking Skeleton

---
+++ ChattingApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using ChattingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChattingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
